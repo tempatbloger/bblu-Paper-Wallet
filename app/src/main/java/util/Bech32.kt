121d56b56674889ab636ef0f7dce280bd4c69bd9
Ahmad Sobandi <44750187+tempatbloger@users.noreply.github.com>
2026-01-13 15:59:06 +0700

    Delete app directory

--- app/src/main/java/util/Bech32.kt
+++
@@ -1,7 +0,0 @@
-package org.bblu.wallet.util
-
-object Bech32 {
-    fun encode(hrp: String, data: ByteArray): String {
-        return "$hrp" + "1" + data.joinToString("") { "%02x".format(it) }
-    }
-}
